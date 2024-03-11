1f51ca4a00f6b3d1fbb602db4618f7972e839c84
John Laffey <laffeychem@gmail.com>
2024-03-11 14:05:20 -0400

    first commit

---
+++ API/Models/Domain/Difficulty.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ API/Models/Domain/Region.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionUrl { get; set; }
+    }
+}
